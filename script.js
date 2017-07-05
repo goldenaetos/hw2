48edf4b62590a45aa20784ab170e03b69428d29e
Vladimir_Kirillov <goldenaetos@gmail.com>
2017-07-05 09:33:40 +0300

    Adding general switch

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 alert(`Добро пожаловать в базу данных фильмов!
 Введите «help» для получения списка команд`)
+
+do {
+    const cmd = prompt("Введите команду.")
+
+    if (cmd == "exit") break
+    
+    switch (cmd) {
+        case "help":
+            alert("Under construction")
+            break
+        case "show":
+            alert("Under construction")
+            break
+        case "addfilm":
+            alert("Under construction")
+        case "amount":
+            alert("Under construction")
+            break
+        case "exit":
+            break
+    }
+} while (true)
